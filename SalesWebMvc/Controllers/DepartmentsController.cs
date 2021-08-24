a788e952dc16738f4c8d19e8f6f963751bb4decb
ducorreia <du.correia.ga@gmail.com>
2021-08-24 14:51:17 +0100

    Fixing bugs in the class Model-Controller-View - Department

--- SalesWebMvc/Controllers/DepartmentsController.cs
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -12,6 +12,8 @@ namespace SalesWebMvc.Controllers
         public IActionResult Index()
         {
             List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name ="Eletronics"});
+            list.Add(new Department { Id = 2, Name = "Fashion" });
             return View(list);
         }
     }
